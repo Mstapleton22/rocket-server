0f4820a272fef5b45ffd3adf42e4162716d92be9
jculver1 <jculver1900@gmail.com>
2019-06-10 13:43:04 -0600

    added configures

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const environment = process.env.NODE_ENV || 'development'
 const knexConfig = require('./knexfile.js')[environment]
 const knex = require('knex')(knexConfig)
+
 require('dotenv').config() 
 
 const bodyParser = require('body-parser')
