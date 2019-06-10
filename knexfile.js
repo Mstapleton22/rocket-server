9cb44d71598e064bc25271bae1d87b7e513e32c5
jculver1 <jculver1900@gmail.com>
2019-06-10 10:19:59 -0600

    added production staging

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,9 @@ module.exports = {
     connection: 'postgres://localhost/rocket'
   },
   production: {
-    client: 'pg', // says that we want to use knex with postgresql
-    connection: process.env.DATABASE_URL
-  }
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://ijqxrandvnsjtq:80e325c0fae2ed0f6a37a864fa5e46bdc5ca5d81368c0d0ecb30d3c9987d702a@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dklgfu4r61725'
+  },
+ 
 }
 
