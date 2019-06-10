bf71df158ab742ecebe26d288d14a03b2125aa9b
jculver1 <jculver1900@gmail.com>
2019-06-10 13:20:48 -0600

    added database name

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://ijqxrandvnsjtq:80e325c0fae2ed0f6a37a864fa5e46bdc5ca5d81368c0d0ecb30d3c9987d702a@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dklgfu4r61725'
+    connection: process.env.DATABASE_URL || ' postgres://ijqxrandvnsjtq:80e325c0fae2ed0f6a37a864fa5e46bdc5ca5d81368c0d0ecb30d3c9987d702a@ec2-54-83-9-36.compute-1.amazonaws.com:5432/dklgfu4r61725'
   },
  
 }
