0b16cca50074526c6bb7e6bf466e1ce7869deb82
Mstapleton22 <mike.stapleton2@gmail.com>
2019-06-17 09:59:17 -0600

    found errors during wiring to FE

--- seeds/contact.js
+++ seeds/contact.js
@@ -3,7 +3,7 @@ exports.seed = function (knex, Promise) {
   return knex('contact').del()
     .then(function () {
       return knex('contact').insert([
-        { id: 1, hours: 'Hours of Operation: 7 a.m - 6 p.m daily', address: 'Address: 5000 East Alameda Avenue, Denver, Colorado 80249', phone: 'Phone: (303)338-7012', email: 'edwards@augustanadenver.org' },
+        { id: 0, hours: 'Hours of Operation: 7 a.m - 6 p.m daily', address: 'Address: 5000 East Alameda Avenue, Denver, Colorado 80249', phone: 'Phone: (303)338-7012', email: 'edwards@augustanadenver.org' },
       ]);
     });
 };
