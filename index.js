8c3ac9043647eb13596222117515ceddc3526d3e
jculver1 <jculver1900@gmail.com>
2019-06-25 09:41:33 -0600

    added carousel put request

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ app.put('/staff/:id', (req, res, next) => {
   })
 })
 
+app.put('/carousel/:id', (req, res, next) => {
+  knex('carousel').update(req.body).where('id', req.params.id).returning('*')
+  .then((rows) => {
+    res.send(200)
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
 app.get('/carousel', (req, res) => {
   knex('carousel')
     .then((rows) => {
