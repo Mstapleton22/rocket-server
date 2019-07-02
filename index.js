8bf5f4f3d825ac15a8e718e418c78aec219b5c3b
jculver1 <jculver1900@gmail.com>
2019-07-02 16:16:13 -0600

    add comment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const environment = process.env.NODE_ENV || 'development'
 const knexConfig = require('./knexfile.js')[environment]
 const knex = require('knex')(knexConfig)
 const bodyParser = require('body-parser')
-
+//add comment
 app.use(bodyParser.urlencoded({extend:true}));
 
 app.use(bodyParser.json())
