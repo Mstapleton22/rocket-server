2b57814938c8cb6bfab276417fac66190b479347
jculver1 <jculver1900@gmail.com>
2019-07-02 19:22:59 -0600

    added control access

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.use(bodyParser.urlencoded({extend:true}));
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // allow preflight
+  if (req.method === 'OPTIONS') {
+      res.send(200);
+  } else {
+      next();
+  }
+})
+
 
 const mailer = require("./controllers/mailer");
 const sign_s3 = require('./controllers/sign_s3');
