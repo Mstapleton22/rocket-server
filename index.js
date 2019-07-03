54b97e9a1b17fbe482d74762b84ec8520a0fe323
jculver1 <jculver1900@gmail.com>
2019-07-02 19:30:01 -0600

    added cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extend:true}));
 
 app.use(bodyParser.json())
-// app.use(cors())
+app.use(cors())
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
