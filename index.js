ed67da03e1a54c8ed964537092254e297a623e20
jculver1 <jculver1900@gmail.com>
2019-07-02 19:25:30 -0600

    made new policy

--- index.js
+++ index.js
@@ -10,19 +10,14 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extend:true}));
 
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  // allow preflight
-  if (req.method === 'OPTIONS') {
-      res.send(200);
-  } else {
-      next();
-  }
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
 
 
 const mailer = require("./controllers/mailer");
