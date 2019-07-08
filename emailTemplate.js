a7b8d074737c2d8267675011d269060c27dbb40a
jculver1 <jculver1900@gmail.com>
2019-07-08 15:27:49 -0600

    included array data to waitlist application

--- emailTemplate.js
+++ emailTemplate.js
@@ -3,24 +3,29 @@ const PDFDocument =require('pdfkit')
 module.exports.waitinglist = (data)=> {
   const doc = new PDFDocument();
 
+  styles = {
+    width: 410,
+    align: 'left'
+  }
 
-  doc.text(`Name: ${data.name}`, {
-    width: 410,
-    align: 'left'
-  }
-  );
+  doc.text(`Date of Inquiry: ${data.inquiry_date}`, styles);
   doc.moveDown()
-  doc.text(`Name: ${data.name}`, {
-    width: 410,
-    align: 'left'
-  }
-  );
+  doc.text(`Tour Date: ${data.tour_date}`, styles)
+  doc.moveDown()
+  doc.text(`Requested Start Date: ${data.requested_date}`)
+  doc.moveDown()
+  doc.text(`Classroom Selected: ${data.chosen_classroom}`)
+  doc.moveDown()
+  doc.text(`The following were circled: ${
+    data.circled.map(item => {
+      return ` ${item}`
+    })
+  }`) 
+
+  doc.moveDown()
+  doc.text(`Name: ${data.name}`, styles);
   doc.moveDown();
-  doc.text(`Name: ${data.email}`, {
-    width: 410,
-    align: 'left'
-  }
-  );
+  doc.text(`Name: ${data.email}`, styles);
   
   doc.end();
   return doc 
