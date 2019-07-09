0534b41ae671068cbc3ff77948e7427ff9da191d
jculver1 <jculver1900@gmail.com>
2019-07-09 17:45:34 -0600

    updated info

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
 module.exports = {
 
-  development: {
-    client: 'pg', // says that we want to use knex with postgresql
-    connection: process.env.DATABASE_URL ||'postgres://localhost/rocket'
-  },
-  
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://zrhfiidejnfnte:e6c7d371116514c4a23c6933baf09bca3376c7872b359e024b2695eca7af1550@ec2-174-129-227-205.compute-1.amazonaws.com:5432/d7bduu9087gn0m'
 
   },
- 
+
+  development: {
+    client: 'pg', // says that we want to use knex with postgresql
+    connection: process.env.DATABASE_URL ||'postgres://localhost/rocket'
+  },
+  
 }
 
