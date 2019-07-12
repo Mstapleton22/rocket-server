ecd1abffb59659fb7e3b6e62a7b735df1cdafbeb
jculver1 <jculver1900@gmail.com>
2019-07-12 10:28:02 -0600

    fixed seesd

--- seeds/mission_vision.js
+++ seeds/mission_vision.js
@@ -4,7 +4,7 @@ exports.seed = function (knex, Promise) {
     return knex('mission_vision').del()
       .then(function () {
         // Inserts seed entries
-        return knex('parentHandbook').insert([
+        return knex('mission_vision').insert([
           { id: 1, name: 'mission', body: 'Augustana Early Learning Center serves children and their families through high quality, affordable care and education. We welcome children from diverse cultures, faith traditions, socioeconomic backgrounds, and national origins. We provide learning experiences that foster developmentally-appropriate growth to teach children to love themselves and others.' },
           { id: 2, name: 'vision', body: 'Augustana Early Learning Center is a community outreach ministry of Augustana Lutheran Church with a vision to love children and inspire them to become engaged, emotionally-intelligent, and curious learners.' },
         ]);
