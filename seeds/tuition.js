d6ccc92cb9947ab7d861fbdef434d129199537bb
jculver1 <jculver1900@gmail.com>
2019-07-15 11:52:54 -0600

    added seeds

--- seeds/tuition.js
+++ seeds/tuition.js
@@ -6,7 +6,7 @@ exports.seed = function (knex, Promise) {
       // Inserts seed entries
       return knex('tuition').insert([
         { id: 1, name: 'Toddler and Preschool', image_name: 'tuition/tod-pk.png', image_url: 'https://elcdenver.s3-us-west-1.amazonaws.com/tuition/tod-pk.png' },
-        { id: 1, name: 'K-5th ', image_name: 'tuition/k-5.png', image_url: 'https://elcdenver.s3-us-west-1.amazonaws.com/tuition/k-5.png' }
+        { id: 2, name: 'K-5th ', image_name: 'tuition/k-5.png', image_url: 'https://elcdenver.s3-us-west-1.amazonaws.com/tuition/k-5.png' }
       ]);
     });
 };
