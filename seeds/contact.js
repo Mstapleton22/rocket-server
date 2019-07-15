712dde23bb09f1051a5d991bfcb9177c2cec2035
jculver1 <jculver1900@gmail.com>
2019-07-15 11:36:09 -0600

    added seeds

--- seeds/contact.js
+++ seeds/contact.js
@@ -3,7 +3,7 @@ exports.seed = function (knex, Promise) {
   return knex('contact').del()
     .then(function () {
       return knex('contact').insert([
-        { id: 1, hours: '7 a.m - 6 p.m daily', address: '5000 East Alameda Avenue, Denver, Colorado 80249', phone: '(303)338-7012', email: 'aelcdenver@gmail.com' },
+        { id: 1, hours: '7 a.m - 6 p.m daily', address: '5000 East Alameda Avenue, Denver, Colorado 80249', phone: '(303) 338-7012', email: 'aelcdenver@gmail.com' },
       ]);
     });
 };
